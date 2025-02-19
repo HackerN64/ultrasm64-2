d5c1f64a55d37ade7b29cbc899bfe767b6d42cc5
username <38231712+mountainflaw@users.noreply.github.com>
2025-02-19 01:53:46 -0500

    Remove lib/src/libgcc

--- lib/src/libgcc/memset.c
+++
@@ -1,9 +0,0 @@
-/* Public domain.  */
-#include <ultra64.h>
-
-void *memset(void *dest, int val, size_t len) {
-    unsigned char *ptr = dest;
-    while (len-- > 0)
-        *ptr++ = val;
-    return dest;
-}
