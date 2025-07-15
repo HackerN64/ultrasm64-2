6bac215bf4ce8d73b953557935c5056966664a7f
a <a>
2025-07-14 22:57:21 -0400

    what

--- lib/n64-libc/n64-assert.c
+++ lib/n64-libc/n64-assert.c
@@ -7,8 +7,8 @@ extern char *assertMsg;
 char assertBuf[256];
 
 void __n64_assert_fail( const char *assertion, const char *file, unsigned int line, const char *fcn ) {
-  n64_snprintf( assertBuf, 256, "%s:%u:%s: Assertion `%s' failed.", file, line, fcn, assertion );
-  assertMsg = &assertBuf[0];
-	n64_printf( "%s:%u: %s: Assertion `%s' failed.\n", file, line, fcn, assertion );
-	n64_abort();
+    n64_snprintf( assertBuf, 256, "%s:%u:%s: Assertion `%s' failed.", file, line, fcn, assertion );
+    assertMsg = &assertBuf[0];
+    n64_printf( "%s:%u: %s: Assertion `%s' failed.\n", file, line, fcn, assertion );
+	  n64_abort();
 }
